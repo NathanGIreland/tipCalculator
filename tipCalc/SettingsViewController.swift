cf9721296ec57907d38c8f8395e7ecac4d70c220
Nathan Ireland <nathangireland@gmail.com>
2019-08-27 10:37:13 -0500

    Added Setting Screen and Show Segue(not fuctioning yet)

---
+++ tipCalc/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  tipCalc
+//
+//  Created by Nathan Ireland on 8/27/19.
+//  Copyright © 2019 Nathan Ireland. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "TIP CALCULATOR"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
